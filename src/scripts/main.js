51494283f95293ead444af33198098b40d6be81c
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-04-25 12:26:39 +0200

    added js buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,16 +4,40 @@ let player1Score, player2Score = 0
 
 const startingPlayer = 1;
 
-function showQuestion() {
-    const $showQuestion = document.querySelector('.quiz__question');
-
-    const randomIndex = Math.floor(Math.random() * questionArray.length)    
-    const randomQuestion = questionArray[randomIndex]
-    $showQuestion.innerHTML = randomQuestion.question;
-}
 
 function game() {
+
+    startingPlayer = initButtons();
+    let currentAnswer = null;
+
     const $showTurn = document.querySelector('.quiz__player-turn');
     $showTurn.innerHTML = `Speler ${startingPlayer} aan de beurt`
+
+    const randomIndex = Math.floor(Math.random() * questionArray.length)    
+    showQuestion(randomIndex)    
 }
-showQuestion()
+
+function showQuestion(randomIndex) {
+    const $showQuestion = document.querySelector('.quiz__question');
+    $showQuestion.innerHTML = questionArray[randomIndex].question;
+}
+
+function initButtons() {
+    const $trueButton = document.querySelector(".button--true");
+    const $falseButton = document.querySelector(".button--false");
+
+    $trueButton.addEventListener('click', () => {
+        console.log("true");
+        startingPlayer === 1 ? 2 : 1;
+    })
+
+    $falseButton.addEventListener('click', () => {
+        console.log("false");
+        startingPlayer === 1 ? 2 : 1;
+    })
+
+    return startingPlayer;
+}
+
+game();
+
