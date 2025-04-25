b1ea7fd5f1fef6d4573a9c54c90283467b9f748f
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-04-25 13:56:00 +0200

    updated game js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,24 +2,39 @@ import questionArray from "../data/questions.js"
 
 let player1Score, player2Score = 0
 
-const startingPlayer = 1;
+let currentPlayer = 1;
+let currentAnswer = null
 
 
 function game() {
+    playerTurn(1);
 
-    startingPlayer = initButtons();
-    let currentAnswer = null;
-
-    const $showTurn = document.querySelector('.quiz__player-turn');
-    $showTurn.innerHTML = `Speler ${startingPlayer} aan de beurt`
-
-    const randomIndex = Math.floor(Math.random() * questionArray.length)    
-    showQuestion(randomIndex)    
+   
 }
 
-function showQuestion(randomIndex) {
-    const $showQuestion = document.querySelector('.quiz__question');
-    $showQuestion.innerHTML = questionArray[randomIndex].question;
+
+
+
+
+
+
+function playerTurn(playerId) {
+    const answer = null
+    // show player turn
+    showTurn(playerId);
+
+    // generate new question
+    const randomIndex = Math.floor(Math.random() * questionArray.length);
+    showQuestion(randomIndex);
+
+    // check if buttons are pressed
+    initButtons();
+    
+    checkAnswer(answer, randomIndex);
+
+    // check if answer is correct
+    // add points to user
+    // switch turn
 }
 
 function initButtons() {
@@ -27,17 +42,28 @@ function initButtons() {
     const $falseButton = document.querySelector(".button--false");
 
     $trueButton.addEventListener('click', () => {
-        console.log("true");
-        startingPlayer === 1 ? 2 : 1;
+        console.log("pressed true");
+        answer = true;
     })
 
     $falseButton.addEventListener('click', () => {
-        console.log("false");
-        startingPlayer === 1 ? 2 : 1;
+        console.log("pressed false");
+        answer = false;
     })
+}
 
-    return startingPlayer;
+function showQuestion(randomIndex) {
+    const $showQuestion = document.querySelector('.quiz__question');
+    $showQuestion.innerHTML = questionArray[randomIndex].question;
+}
+
+function showTurn() {
+    const $showTurn = document.querySelector('.quiz__player-turn');
+    $showTurn.innerHTML = `Speler ${currentPlayer} aan de beurt`
+}
+
+function checkAnswer() {
+    if
 }
 
 game();
-
