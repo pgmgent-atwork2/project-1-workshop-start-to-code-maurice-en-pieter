546a53338abee989b025cd29cad8052e1eb67fee
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-04-25 11:53:32 +0200

    added questions data

---
+++ src/data/questions.js
@@ -0,0 +1,83 @@
+export default questions = [
+    {
+      question: "De aarde draait om de zon.",
+      answer: true
+    },
+    {
+      question: "Vleermuizen zijn blind.",
+      answer: false
+    },
+    {
+      question: "Water kookt bij 100 graden Celsius op zeeniveau.",
+      answer: true
+    },
+    {
+      question: "Een octopus heeft zes armen.",
+      answer: false
+    },
+    {
+      question: "Nederland grenst aan Duitsland en België.",
+      answer: true
+    },
+    {
+      question: "De hoofdstad van Australië is Sydney.",
+      answer: false
+    },
+    {
+      question: "Er zijn zeven continenten op aarde.",
+      answer: true
+    },
+    {
+      question: "Lood is lichter dan water.",
+      answer: false
+    },
+    {
+      question: "De menselijke longen kunnen zuurstof uit water halen.",
+      answer: false
+    },
+    {
+      question: "De Mona Lisa is geschilderd door Leonardo da Vinci.",
+      answer: true
+    },
+    {
+      question: "Bananen groeien aan bomen.",
+      answer: false
+    },
+    {
+      question: "Het menselijk lichaam heeft 206 botten.",
+      answer: true
+    },
+    {
+      question: "De Eiffeltoren staat in Rome.",
+      answer: false
+    },
+    {
+      question: "Haaien zijn zoogdieren.",
+      answer: false
+    },
+    {
+      question: "Zilver is een beter geleider dan koper.",
+      answer: true
+    },
+    {
+      question: "Mars heeft twee manen.",
+      answer: true
+    },
+    {
+      question: "Een jaar op Venus is korter dan een dag op Venus.",
+      answer: true
+    },
+    {
+      question: "Het getal nul is even.",
+      answer: true
+    },
+    {
+      question: "Een kameleon verandert van kleur om zich te camoufleren.",
+      answer: false
+    },
+    {
+      question: "Albert Einstein won de Nobelprijs voor zijn relativiteitstheorie.",
+      answer: false
+    }
+]
+  
---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+import question from "../data/questions.js"
+let player1Score, player2Score = 0
+
+function showQuestions() {
+    document.querySelector
+}
