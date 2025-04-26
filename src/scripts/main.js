8a8609ff9318a7f08f306487b8727752404c090e
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-04-26 22:15:12 +0200

    updated variable and functions name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,58 +2,68 @@ import questionArray from "../data/questions.js";
 
 let player1Score = 0, player2Score = 0;
 let currentPlayer = 1;
-let currentCorrectAnswer = null;
+let currentAnswer = null;
+let isWaiting = false;
 
-const $showQuestion = document.querySelector('.quiz__question');
-const $showTurn = document.querySelector('.quiz__player-turn');
-
+const $questionDisplay = document.querySelector('.quiz__question');
+const $playerTurnDisplay = document.querySelector('.quiz__player-turn');
 const $trueButton = document.querySelector(".button--true");
 const $falseButton = document.querySelector(".button--false");
 
-function game() {
+function startGame() {
     initButtons();
-    visualizePoints()
+    updatePoints();
     playerTurn();
 }
 
 function initButtons() {
-    $trueButton.addEventListener("click", () => checkAnswer(true, currentCorrectAnswer));
+    $trueButton.addEventListener("click", () => {
+        if (!isWaiting) {
+            checkAnswer(true, currentAnswer);
+        }
+    });
 
-    $falseButton.addEventListener("click", () => checkAnswer(false, currentCorrectAnswer));
+    $falseButton.addEventListener("click", () => {
+        if (!isWaiting) {
+            checkAnswer(false, currentAnswer);
+        }
+    });
 }
 
 function playerTurn() {
-    document.querySelector(".quiz__answer").innerHTML = ""
-    $showTurn.innerHTML = `Speler ${currentPlayer} is aan de beurt`;
+    document.querySelector(".quiz__answer").innerHTML = "";
+    $playerTurnDisplay.innerHTML = `Speler ${currentPlayer} is aan de beurt`;
 
     const randomQuestion = questionArray[Math.floor(Math.random() * questionArray.length)];
-    currentCorrectAnswer = randomQuestion.answer;
-    $showQuestion.innerHTML = randomQuestion.question;
+    currentAnswer = randomQuestion.answer;
+    $questionDisplay.innerHTML = randomQuestion.question;
 
-    visualizePoints();
+    updatePoints();
+    isWaiting = false;
 }
 
 function checkAnswer(givenAnswer, correctAnswer) {
-    const $showAnswer = document.querySelector(".quiz__answer");
+    isWaiting = true;
+    const $answerDisplay = document.querySelector(".quiz__answer");
+
     if (givenAnswer === correctAnswer) {
         currentPlayer === 1 ? player1Score++ : player2Score++;
-        $showAnswer.innerHTML = `Correct! Speler ${currentPlayer} +1 punt`
-        
+        $answerDisplay.innerHTML = `Correct! Speler ${currentPlayer} +1 punt`;
     } else {
         currentPlayer === 1 ? player2Score++ : player1Score++;
-        $showAnswer.innerHTML = `Incorrect! Speler ${currentPlayer === 1 ? 2 : 1} +1 punt`
+        $answerDisplay.innerHTML = `Incorrect! Speler ${currentPlayer === 1 ? 2 : 1} +1 punt`;
     }
 
     currentPlayer = currentPlayer === 1 ? 2 : 1;
-    setTimeout(() => playerTurn(), 1000);
+
+    setTimeout(() => { playerTurn()}, 1000);
 }
 
-function visualizePoints() {
-    const playerProgress = document.querySelectorAll('.progress');
+function updatePoints() {
+    const playerPoints = document.querySelectorAll('.progress');
     const scores = [player1Score, player2Score];
 
-    //loop over scores and visualize them
-    playerProgress.forEach((progress, index) => {
+    playerPoints.forEach((progress, index) => {
         for (const child of progress.children) {
             child.classList.remove('current');
         }
@@ -62,7 +72,6 @@ function visualizePoints() {
         }
     });
 
-    // check for winner
     if (player1Score >= 10) {
         playerWins(1);
     } else if (player2Score >= 10) {
@@ -71,10 +80,10 @@ function visualizePoints() {
 }
 
 function playerWins(playerId) {
-    $trueButton.style.display = "none"
-    $falseButton.style.display = "none"
-    $showQuestion.innerHTML = `Speler ${playerId} wint het spel!`
-    $showTurn.innerHTML = "<a class='quiz__link' href='/game.html'>Speel nog eens</a>";
+    $trueButton.style.display = "none";
+    $falseButton.style.display = "none";
+    $questionDisplay.innerHTML = `Speler ${playerId} wint het spel!`;
+    $playerTurnDisplay.innerHTML = "<a class='quiz__link' href='/game.html'>Speel nog eens</a>";
 }
 
-game();
+startGame();
