70ee50f6192e219638e66536b2ee62c512df23f5
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-04-26 22:04:42 +0200

    cleaned up js code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,36 +13,24 @@ const $falseButton = document.querySelector(".button--false");
 function game() {
     initButtons();
     visualizePoints()
-    playerTurn(currentPlayer);
+    playerTurn();
 }
 
 function initButtons() {
-    $trueButton.addEventListener("click", () => {
-        handleAnswer(true);
-    });
+    $trueButton.addEventListener("click", () => checkAnswer(true, currentCorrectAnswer));
 
-    $falseButton.addEventListener("click", () => {
-        handleAnswer(false);
-    });
+    $falseButton.addEventListener("click", () => checkAnswer(false, currentCorrectAnswer));
 }
 
-function playerTurn(playerId) {
+function playerTurn() {
     document.querySelector(".quiz__answer").innerHTML = ""
     $showTurn.innerHTML = `Speler ${currentPlayer} is aan de beurt`;
 
-    const randomIndex = Math.floor(Math.random() * questionArray.length);
-    const question = questionArray[randomIndex];
-
-    currentCorrectAnswer = question.answer;
-    $showQuestion.innerHTML = questionArray[randomIndex].question;
+    const randomQuestion = questionArray[Math.floor(Math.random() * questionArray.length)];
+    currentCorrectAnswer = randomQuestion.answer;
+    $showQuestion.innerHTML = randomQuestion.question;
 
     visualizePoints();
-
-    console.log(player1Score, player2Score);
-}
-
-function handleAnswer(givenAnswer) {
-    checkAnswer(givenAnswer, currentCorrectAnswer);
 }
 
 function checkAnswer(givenAnswer, correctAnswer) {
@@ -52,48 +40,33 @@ function checkAnswer(givenAnswer, correctAnswer) {
         $showAnswer.innerHTML = `Correct! Speler ${currentPlayer} +1 punt`
         
     } else {
-        console.log('Incorrect');
         currentPlayer === 1 ? player2Score++ : player1Score++;
         $showAnswer.innerHTML = `Incorrect! Speler ${currentPlayer === 1 ? 2 : 1} +1 punt`
     }
 
     currentPlayer = currentPlayer === 1 ? 2 : 1;
-    setTimeout(() => playerTurn(currentPlayer), 500);
+    setTimeout(() => playerTurn(), 1000);
 }
-
-game();
-
 
 function visualizePoints() {
     const playerProgress = document.querySelectorAll('.progress');
-    
-    //Player 1
-    for (const child of playerProgress[0].children) {
-        child.classList.remove('current');
-    }
-    if (player1Score < playerProgress[0].children.length) {
-        playerProgress[0].children[player1Score+1].classList.add('current');
-        playerProgress[0].children[player1Score+1].classList.add('completed');
-    }
+    const scores = [player1Score, player2Score];
 
-    //Player 2
-    for (const child of playerProgress[1].children) {
-        child.classList.remove('current');
-    }
-    if (player2Score < playerProgress[1].children.length) {
-        playerProgress[1].children[player2Score+1].classList.add('current');
-        playerProgress[1].children[player2Score+1].classList.add('completed');
-    }
+    //loop over scores and visualize them
+    playerProgress.forEach((progress, index) => {
+        for (const child of progress.children) {
+            child.classList.remove('current');
+        }
+        if (scores[index] < progress.children.length) {
+            progress.children[scores[index] + 1].classList.add('current', 'completed');
+        }
+    });
 
-    // check winner
+    // check for winner
     if (player1Score >= 10) {
-        console.log('player 1 wins');
         playerWins(1);
-        
     } else if (player2Score >= 10) {
-        console.log('player 2 wins');
         playerWins(2);
-        
     }
 }
 
@@ -101,5 +74,7 @@ function playerWins(playerId) {
     $trueButton.style.display = "none"
     $falseButton.style.display = "none"
     $showQuestion.innerHTML = `Speler ${playerId} wint het spel!`
-    $showTurn.innerHTML = "<a class=='quiz__link' href='/game.html'>Speel nog eens</a>"
+    $showTurn.innerHTML = "<a class='quiz__link' href='/game.html'>Speel nog eens</a>";
 }
+
+game();
