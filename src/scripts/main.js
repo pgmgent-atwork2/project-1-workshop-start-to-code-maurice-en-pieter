4770d9819d21cdc800025a84b21121ef83442ff5
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-04-26 21:50:04 +0200

    added winner and game ending

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,9 @@ let player1Score = 0, player2Score = 0;
 let currentPlayer = 1;
 let currentCorrectAnswer = null;
 
+const $showQuestion = document.querySelector('.quiz__question');
+const $showTurn = document.querySelector('.quiz__player-turn');
+
 const $trueButton = document.querySelector(".button--true");
 const $falseButton = document.querySelector(".button--false");
 
@@ -25,15 +28,17 @@ function initButtons() {
 
 function playerTurn(playerId) {
     document.querySelector(".quiz__answer").innerHTML = ""
-    showTurn(playerId);
+    $showTurn.innerHTML = `Speler ${currentPlayer} is aan de beurt`;
 
     const randomIndex = Math.floor(Math.random() * questionArray.length);
     const question = questionArray[randomIndex];
 
     currentCorrectAnswer = question.answer;
-    showQuestion(randomIndex);
+    $showQuestion.innerHTML = questionArray[randomIndex].question;
 
     visualizePoints();
+
+    console.log(player1Score, player2Score);
 }
 
 function handleAnswer(givenAnswer) {
@@ -44,25 +49,16 @@ function checkAnswer(givenAnswer, correctAnswer) {
     const $showAnswer = document.querySelector(".quiz__answer");
     if (givenAnswer === correctAnswer) {
         currentPlayer === 1 ? player1Score++ : player2Score++;
-        $showAnswer.innerHTML = "Correct!"
+        $showAnswer.innerHTML = `Correct! Speler ${currentPlayer} +1 punt`
+        
     } else {
         console.log('Incorrect');
         currentPlayer === 1 ? player2Score++ : player1Score++;
-        $showAnswer.innerHTML = "Incorrect!"
+        $showAnswer.innerHTML = `Incorrect! Speler ${currentPlayer === 1 ? 2 : 1} +1 punt`
     }
 
     currentPlayer = currentPlayer === 1 ? 2 : 1;
-    setTimeout(() => playerTurn(currentPlayer), 1000);
-}
-
-function showQuestion(randomIndex) {
-    const $showQuestion = document.querySelector('.quiz__question');
-    $showQuestion.innerHTML = questionArray[randomIndex].question;
-}
-
-function showTurn() {
-    const $showTurn = document.querySelector('.quiz__player-turn');
-    $showTurn.innerHTML = `Speler ${currentPlayer} is aan de beurt`;
+    setTimeout(() => playerTurn(currentPlayer), 500);
 }
 
 game();
@@ -88,4 +84,22 @@ function visualizePoints() {
         playerProgress[1].children[player2Score+1].classList.add('current');
         playerProgress[1].children[player2Score+1].classList.add('completed');
     }
+
+    // check winner
+    if (player1Score >= 10) {
+        console.log('player 1 wins');
+        playerWins(1);
+        
+    } else if (player2Score >= 10) {
+        console.log('player 2 wins');
+        playerWins(2);
+        
+    }
+}
+
+function playerWins(playerId) {
+    $trueButton.style.display = "none"
+    $falseButton.style.display = "none"
+    $showQuestion.innerHTML = `Speler ${playerId} wint het spel!`
+    $showTurn.innerHTML = "<a class=='quiz__link' href='/game.html'>Speel nog eens</a>"
 }
