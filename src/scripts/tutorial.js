d124f7f52221163cc4425ee2fe7e5bef6d6b67b5
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-05-02 10:17:03 +0200

    updated copy button

--- src/scripts/tutorial.js
+++ src/scripts/tutorial.js
@@ -1,7 +1,8 @@
 for(let i = 1; i<16; i++) {
-    const button = document.getElementById(`copy${i}`);
-    button.addEventListener('click', () => {
-        const code = document.getElementById(`code${i}`).innerText;
-        navigator.clipboard.writeText(code)
+    const $button = document.getElementById(`copy${i}`);
+    $button.addEventListener('click', () => {
+        const $code = document.getElementById(`code${i}`).innerText;
+        navigator.clipboard.writeText($code)
+        $button.innerHTML = 'Copied'
     })
 }
